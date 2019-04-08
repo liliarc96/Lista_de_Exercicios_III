ad89a39c4074b159f16cd582b50f39642bf8dc65
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-04-07 23:53:22 -0300

    Update minhaString.h

--- minhaString.h
+++ minhaString.h
@@ -1,5 +1,22 @@
 #ifndef MINHASTRING_H
 #define MINHASTRING_H
-//#include guards
+/*
+*strcpy - Copia string para outro array char
+
+*strcmp - Comparação entre duas strings
+
+*strcat - Concatenação de duas strings
+
+*strlen - Retorna tamanho da string
+*/
+
+//strcpy()
+void copiaString(char* destino, char* original){
+	int i; // Índice contador
+
+	for(i = 0; i < sizeof(original); i++){
+		destino[i]= original[i]; // Destino recebe o que está no índice de Original
+	}
+}
 
 #endif /* MINHASTRING_H */
