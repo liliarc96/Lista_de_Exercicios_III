ddf37ceb234e6cacd1fde2c53d5bbf64fcc4ca9e
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-04-08 00:22:43 -0300

    Update minhaString.h

--- minhaString.h
+++ minhaString.h
@@ -19,4 +19,27 @@ void copiaString(char* destino, char* original){
 	}
 }
 
+//strcmp()
+int comparaString(char* string1, char* string2){
+	int i; // Índice contador
+	int maiorprimeira = 0;
+	int maiorsegunda = 0;
+
+	for(i = 0;i < sizeof(string1); i++){
+		if(string1[i] > string2[i]){
+			maiorprimeira++;
+		}else if(string1[i] < string2[i]){
+			maiorsegunda++;
+		}
+		if(maiorprimeira > maiorsegunda){
+			return 1;
+		}else if(maiorsegunda > maiorprimeira){
+			return -1;
+		}else{
+			//As duas são iguais
+			return 0;
+		}
+	}
+}
+
 #endif /* MINHASTRING_H */
