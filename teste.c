1d66336395cada4c475aaac762603c9530723732
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-04-14 22:31:41 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -5,9 +5,10 @@
 
 int main()
 {
-   char name[] = "Hello";
-   char surname[] = "World";
-
+   char name[] = "abcde";
+   char surname[] = "ABCDE";
+   
+   printf("-----------Inicio do teste 1-----------\n");
    printf("%s\n", name );
    printf("%s\n", surname );
 
@@ -24,5 +25,13 @@ int main()
       Hello
       Hello*/
    
+   printf("-----------Fim do teste 1-----------\n\n\n\n");
+   printf("-----------Inicio do teste 2-----------\n");
+   int teste2;
+   teste2 = comparaString(name,surname);
+
+   printf("Retorno foi %d", teste2);
+   printf("-----------Fim do teste 2-----------\n\n\n\n");
+   printf("-----------Inicio do teste 3-----------\n");
    return 0;
 }
